46c7ee59975b9db0df0124c3c23800e631936cde
Mirshad Oz <mirshadoz@mirshads-macbook-pro.local>
2018-06-04 23:37:14 +0300

    Initial Commit

---
+++ Todoey App/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Todoey App
+//
+//  Created by Mirshad Oz on 6/4/18.
+//  Copyright © 2018 Mirshad Oz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Button is pressed!")
+    }
+    
+}
+
